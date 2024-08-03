9bb7d55aa8cd5d2168923555f38494bea4bd0b4e
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-04 00:49:46 +0600

    header responsive done

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,34 +5,34 @@ import logo from "../../assets/images/logo.png";
 const Header = () => {
     return (
         <header>
-            <div className="max-width h-24 flex justify-between items-center">
+            <div className="max-width h-14 md:h-24 flex justify-between items-center">
                 {/* logo  */}
                 <div className="flex items-center">
                     <img
-                        className="w-20"
+                        className="w-10 md:w-20"
                         src={logo}
                         alt="Logo of E6 Electronics Ltd"
                     />
                     <div>
-                        <h2 className="uppercase font-bold text-[26px] leading-tight text-primary">
+                        <h2 className="uppercase font-bold text-sm md:text-[26px] leading-tight text-primary">
                             E6 Electrical LTD
                         </h2>
-                        <p className="uppercase text-[12px]">
+                        <p className="uppercase text-[7px] md:text-[12px]">
                             Excellence In Electrical services
                         </p>
                     </div>
                 </div>
                 {/* contact information */}
-                <div className="flex items-center gap-3">
-                    <IoCall className="text-4xl text-primary" />
+                <div className="flex items-center gap-2 md:gap-3">
+                    <IoCall className="text-xl md:text-4xl text-primary" />
                     <span>
                         <a
-                            className="text-[26px] font-semibold leading-none"
+                            className="text-base md:text-[26px] font-semibold leading-none"
                             href="tel:1234567890"
                         >
                             1234567890
                         </a>
-                        <p className="text-sm">Mon to Sat 8:00 AM to 6:00 PM</p>
+                        <p className="text-[8px] md:text-sm">Mon to Sat 8:00 AM to 6:00 PM</p>
                     </span>
                 </div>
             </div>
