8dd01cf9a3af94c08d3aa366644c129c86fbe541
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-04 22:16:00 +0600

    navbar added

--- src/AllRoutes/Layout.jsx
+++ src/AllRoutes/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 import Header from "../components/shared/Header";
 import Footer from "../components/shared/footer/Footer";
+import Navbar from "../components/shared/Navbar";
 
 const Layout = () => {
   return (
     <>
       <Header />
+      <Navbar />
       <Outlet />
       <ScrollRestoration />
       <Footer />
