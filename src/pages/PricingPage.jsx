351a48332e3db8199e25cb384c603652828bcfaf
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-06 17:45:33 +0600

    pricing done

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import Banner from '../components/shared/Banner';
+import Banner from '../components/shared/Banner'
+import Certification from '../components/Certification';
+import ServicesPricing from '../components/ServicesPricing';
 
 const PricingPage = () => {
     const pageName = "Our Pricing";
@@ -7,6 +9,11 @@ const PricingPage = () => {
         <>
             {/* ==== Banner Section ==== */}
             <Banner pageName={pageName} />
+            {/* ==== Pricing Section ==== */}
+            <div className="flex lg:flex-row flex-col my-28 gap-16 lg:gap-6 max-width">
+            <Certification />
+            <ServicesPricing />
+            </div>
 
         </>
     );
