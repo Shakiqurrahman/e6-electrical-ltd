a199f6e5fb51f34dd7a138e178ecc6ad54b5c05d
devshakil <sar4shakil@gmail.com>
2024-08-07 12:10:49 +0600

    added new slider package and solved the problem of lightbox and image slider

--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -1,38 +1,44 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
 import product1 from "../assets/images/product1.png";
 import product2 from "../assets/images/product2.png";
 import product3 from "../assets/images/product3.png";
 import product4 from "../assets/images/product4.png";
 import product5 from "../assets/images/product5.png";
-// import product6 from "../assets/images/product6.png"
+import product6 from "../assets/images/product6.png";
 
 function OurProducts() {
+  const imgs = [product1, product2, product3, product4, product5, product6];
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="flex items-center gap-4 mt-6 text-center">
-      <img
-        src={product1}
-        alt=""
-        className="basis-1/3 sm:basis-1/4 md:basis-1/5 h-[100px] object-contain"
-      />
-      <img
-        src={product2}
-        alt=""
-        className="basis-1/3 sm:basis-1/4 md:basis-1/5 h-[100px] object-contain"
-      />
-      <img
-        src={product3}
-        alt=""
-        className="basis-1/3 sm:basis-1/4 md:basis-1/5 h-[100px] object-contain"
-      />
-      <img
-        src={product4}
-        alt=""
-        className="basis-1/3 sm:basis-1/4 md:basis-1/5 h-[100px] object-contain"
-      />
-      <img
-        src={product5}
-        alt=""
-        className="basis-1/3 sm:basis-1/4 md:basis-1/5 h-[100px] object-contain"
-      />
+    <div className="mt-6">
+      <Slider {...settings}>
+        {imgs.map((img) => (
+          <div key={Math.random()} className="text-center px-3">
+            <img src={img} alt="" className="mx-auto" />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
--- src/components/WorkGallery.jsx
+++ src/components/WorkGallery.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
 import gallery1 from "../assets/images/gallery/gallery1.jpg";
 import gallery2 from "../assets/images/gallery/gallery2.jpg";
 import gallery3 from "../assets/images/gallery/gallery3.jpg";
@@ -20,22 +22,53 @@ function WorkGallery() {
     gallery8,
     gallery9,
   ];
+
+  const [modal, setModal] = useState(false);
+  const [tempImg, setTempImg] = useState("");
+
+  const getImg = (img) => {
+    setTempImg(img);
+    setModal(true);
+  };
+  const closeModal = () => {
+    setModal(false);
+    setTempImg("");
+  };
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-6 flex-wrap gap-5">
-      {imgs.map((img) => (
-        <div
-          key={Math.random()}
-          className="group relative rounded-lg overflow-hidden hover:cursor-pointer"
-        >
+    <>
+      {modal && (
+        <div className="fixed w-full h-screen bg-[rgba(0,0,0,0.7)] p-4 lg:p-50 text-center z-[9999999999] top-0 left-0 flex items-center justify-center backdrop-blur-sm">
+          <button
+            className="fixed top-5 right-5 sm:top-8 sm:right-8 text-white text-3xl z-[9999999999]"
+            onClick={closeModal}
+          >
+            <IoClose />
+          </button>
           <img
-            src={img}
+            src={tempImg}
             alt=""
-            className="block h-[250px] lg:h-[300px] w-full object-cover"
+            className="w-auto max-w-full h-auto max-h-full"
           />
-          <span className="absolute h-full w-full block top-0 left-0 opacity-0 duration-300 group-hover:opacity-100 bg-[rgba(0,0,0,0.3)]"></span>
         </div>
-      ))}
-    </div>
+      )}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 mt-6 flex-wrap gap-5">
+        {imgs.map((img) => (
+          <div
+            key={Math.random()}
+            className="group relative rounded-lg overflow-hidden hover:cursor-pointer"
+            onClick={() => getImg(img)}
+          >
+            <img
+              src={img}
+              alt=""
+              className="block h-[250px] lg:h-[300px] w-full object-cover"
+            />
+            <span className="absolute h-full w-full block top-0 left-0 opacity-0 duration-300 group-hover:opacity-100 bg-[rgba(0,0,0,0.3)]"></span>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
