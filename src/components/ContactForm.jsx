5ed6b6ebfd90d99ed5821233c4caf9e6cf014ea4
Shakiqurrahman <rahmanshakiqur@gmail.com>
2024-08-12 23:22:29 +0600

    email js integrate in only contact page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,11 +6,31 @@ import {
   Textarea,
   Typography,
 } from "@material-tailwind/react";
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
 
 const ContactForm = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_a1vcbab', 'template_1lqfvio', form.current, {
+        publicKey: 'n52gp0xjD6BXOeuKd',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
   return (
     <>
-      <div className="p-6 w-full lg:w-1/2 shadow-card rounded-lg">
+      <div  className="p-6 w-full lg:w-1/2 shadow-card rounded-lg">
         <div className="relative m-0 block text-center">
           <Typography className="mt-4" variant="h3" color="blue-gray">
             Make a Booking
@@ -20,10 +40,11 @@ const ContactForm = () => {
             describe the problem you are having.
           </Typography>
         </div>
+        <form ref={form} onSubmit={sendEmail}>
         <div className="space-y-4 pb-0">
           <div className="flex flex-col md:flex-row gap-4">
             <div className="w-full">
-              <Input color="blue" size="lg" name="name" label="Name" required />
+              <Input color="blue" size="lg" name="from_name" label="Name" required />
             </div>
             <div className="w-full">
               <Input
@@ -40,7 +61,7 @@ const ContactForm = () => {
               <Input
                 color="blue"
                 size="lg"
-                name="email"
+                name="from_email"
                 label="Email"
                 required
               />
@@ -49,7 +70,7 @@ const ContactForm = () => {
               <Input
                 color="blue"
                 size="lg"
-                name="post-code"
+                name="post_code"
                 label="Post Code"
                 required
               />
@@ -58,28 +79,32 @@ const ContactForm = () => {
           <Select
             className="text-[12px] leading-tight text-pretty"
             color="blue"
-            label="Select Version"
+            label="Select service"
+            name="service"
             required
           >
-            <Option>
+            <Option name='Complete Electrical Installation With 10 Year Warranty'>
               Complete Electrical Installation With 10 Year Warranty
             </Option>
-            <Option>Consumer Unit / Fuse Box Replacement / Upgrade </Option>
-            <Option>
+            <Option name='Consumer Unit / Fuse Box Replacement / Upgrade '>Consumer Unit / Fuse Box Replacement / Upgrade </Option>
+            <Option name='Smoke Alarm Certificate for Domestic or Commercial Properties'>
               Smoke Alarm Certificate for Domestic or Commercial Properties{" "}
             </Option>
-            <Option>PAT Testing Certificate </Option>
-            <Option>
+            <Option name='PAT Testing Certificate '>PAT Testing Certificate </Option>
+            <Option name='Emergency Lighting Certificate for Domestic Commercial or
+              Industrial Premises'>
               Emergency Lighting Certificate for Domestic Commercial or
               Industrial Premises
             </Option>
-            <Option>
+            <Option name='Electrical Safety Certificate for Domestic Commercial or
+              Industrial Properties'>
               Electrical Safety Certificate for Domestic Commercial or
               Industrial Properties
             </Option>
           </Select>
           <div>
             <Textarea
+            name="message"
               color="blue"
               label="Details about the issue you're having"
             />
@@ -91,10 +116,9 @@ const ContactForm = () => {
           </Typography>
         </div>
         <div className="mt-4">
-          <Button className="mx-auto w-full py-4 bg-blue-600 text-sm">
-            Send Booking Request
-          </Button>
+          <input type="submit" className="mx-auto w-full py-4 bg-blue-600 text-lg text-white rounded-md" value={'Send Booking Request'}/>
         </div>
+        </form>
       </div>
     </>
   );
